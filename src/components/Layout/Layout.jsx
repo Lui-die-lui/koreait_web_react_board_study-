18d964b92863de7b6c45a82f75e09749292dda3e
Lui-die-lui <lsg05h@naver.com>
2025-08-21 18:47:06 +0900

    feat:board

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,7 +3,7 @@
 import Header from "../Header/Header";
 import * as s from "./Styles";
 
-// Layout 안에 home이 있음 - header 밑에 home(childeren)
+// Layout 안에 home이 있음 - header 밑에 home(childeren) - 자식 컴포넌트
 function Layout({ children }) {
   return (
     <div css={s.layout}>
