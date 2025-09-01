d34dd1500b73bd7b802310e5eacbe7a306b8195f
Lui-die-lui <lsg05h@naver.com>
2025-09-01 19:04:15 +0900

    feat:profile

--- src/pages/Profile/Styles.js
+++ src/pages/Profile/Styles.js
@@ -1,84 +1,85 @@
 import { css } from "@emotion/react";
 
 export const container = css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: 750px;
-  padding: 40px 200px;
-  box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	padding: 40px 200px;
+	box-sizing: border-box;
 `;
 
 export const profileContainer = css`
-  width: 100%;
-  min-height: 700px;
-  display: flex;
-  flex-direction: column;
+	width: 100%;
+	min-height: 700px;
+	display: flex;
+	flex-direction: column;
 `;
 
 export const profileHeader = css`
-  width: 100%;
-  height: 200px;
-  background-color: chocolate;
+	width: 100%;
+	height: 200px;
+	display: flex;
 `;
 
 export const profileMain = css`
-  width: 100%;
-  height: 500px;
-  background-color: cyan;
+	width: 100%;
+	height: 500px;
+	background-color: cyan;
 `;
 
 export const profileImgBox = css`
-  width: 250px;
-  height: 100%;
-  background-color: aliceblue;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+	width: 250px;
+	height: 100%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 
-  & > div {
-    width: 100%;
-    height: 100px;
-    border-radius: 50%;
-    border: 1px solid #bdbdbd;
-    overflow: hidden;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+	& > div {
+		width: 150px;
+		height: 150px;
+		border-radius: 50%;
+		border: 1px solid #dbdbdb;
+		overflow: hidden;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 
-    & > img {
-      width: 100%;
-    }
-
-    & > div {
-      display: flex;
-      gap: 15px;
-    }
-  }
+		& > img {
+			width: 100%;
+		}
+	}
 `;
 
-export const prifileInfoBox = css`
-  width: calc(100%-250px);
-  height: 100%;
-  background-color: darkcyan;
-  padding: 30px 40px;
-  box-sizing: border-box;
-  color: #333;
-  & > h3 {
-    font-size: 24px;
-  }
+export const profileInfoBox = css`
+	width: calc(100% - 250px);
+	height: 100%;
+	padding: 30px 40px;
+	box-sizing: border-box;
+	color: #333;
 
-  & > p {
-    margin: 0;
-  }
+	& > h3 {
+		font-size: 24px;
+	}
 
-  & > button {
-    border: none;
-    padding: 3px 5px;
-    font-size: 11px;
-    font-weight: 600;
-    border-radius: 4px;
-    /* background-color: #0d; */
-  }
+	& > div {
+		display: flex;
+		align-items: center;
+		gap: 15px;
+
+		& > p {
+			margin: 0;
+		}
+
+		& > button {
+			border: none;
+			padding: 3px 5px;
+			font-size: 11px;
+			font-weight: 600;
+			border-radius: 4px;
+			background-color: #0d6efd;
+			color: white;
+			cursor: pointer;
+		}
+	}
 `;
